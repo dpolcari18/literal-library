b38b9a2d47aa0e073017dbdc2480738f3c46cfe1
David Polcari <dpolcari18@gmail.com>
2021-01-30 13:52:18 -0600

    Corrected routing for run file:

---
+++ bin/cli.rb
@@ -0,0 +1,17 @@
+require 'tty-prompt'
+# require_all 'bin/models'
+
+class Cli
+     
+    def self.greet_user
+        
+    end
+
+    def self.librarian
+        
+    end
+
+    def self.student
+
+    end
+end
--- lib/run.rb
+++ lib/run.rb
@@ -1,5 +1,6 @@
 require_relative '../config/environment'
+require_relative '../bin/cli.rb'
 
-
+Cli.greet_user
 
 puts "HELLO WORLD"
