3f7de9f0f30132b7a511c818f1501b4e41932063
David Polcari <dpolcari18@gmail.com>
2021-04-28 16:01:14 -0500

    added option to create new author when adding book

--- bin/cli.rb
+++ bin/cli.rb
@@ -6,11 +6,11 @@ class Cli
     # Greet user - choose library - choose librarian or student
     def self.greet_user
         # choose library name (string)
-        library = @@prompt.select("Welcome! Which library would you like to access?", Library.all.map{|libraries| libraries.name})
+        library = @@prompt.select("\nWelcome! Which library would you like to access?\n", Library.all.map{|libraries| libraries.name})
         # saves library instance that matches chosen library name above (instance)
         @@library = Library.find_by(name: library)
         # choose user type
-        user_type = @@prompt.select("Are you a librarian or a student?", %w(Librarian Student Restart Exit Help))
+        user_type = @@prompt.select("\nAre you a librarian or a student?\n", %w(Librarian Student Restart Exit Help))
         case user_type
         when "Librarian"
             self.librarian_login
@@ -21,44 +21,44 @@ class Cli
         when "Exit"
             return
         when "Help"
-            puts "At any time select Exit to close app or select Restart to go back to the beginning."
+            puts "\nAt any time select Exit to close app or select Restart to go back to the beginning.\n"
             self.greet_user
         end
     end
 
     def self.librarian_login
         # Ask if new or returning librarian
-        new_returning = @@prompt.select("Are you clocking in for work or a new hire?", ["Clocking In", "New Hire", "Restart", "Exit"])
+        new_returning = @@prompt.select("\nAre you clocking in for work or a new hire?\n", ["Clocking In", "New Hire", "Restart", "Exit"])
             case new_returning
             when "Clocking In"
                 # Ask for username and make sure it is in system
-                username = @@prompt.ask("Please enter your username:", required: true)
+                username = @@prompt.ask("\nPlease enter your username:\n", required: true)
                 @@librarian = Librarian.find_by(username: username)
                 if @@librarian
                     # Ask for password and make sure it matches username
                     password = @@prompt.mask("Please enter your password:", required: true)
                     if @@librarian.password == password && @@librarian.library_id == @@library.id
                         # Go to librarian interface
-                        puts "Welcome back to the #{@@library.name} library #{@@librarian.name}!"
+                        puts "\nWelcome back to the #{@@library.name} library #{@@librarian.name}!\n"
                         self.librarian_interface
                     else
                         # Error handling if password doesn't match
-                        puts "Password does not match our records. Please try again."
+                        puts "\nPassword does not match our records. Please try again.\n"
                         self.librarian_login
                     end
                 else
                     # Error handling if librarian is not found
-                    puts "Sorry we don't have you in our system. Please register as a new hire."
+                    puts "\nSorry we don't have you in our system. Please register as a new hire.\n"
                     self.librarian_login
                 end
             when "New Hire"
                 # input name, username, password required to create Librarian instance
-                name = @@prompt.ask("Welcome to onboarding. What is your name?", required: true)
-                username = @@prompt.ask("Please create a username:", required: true)
-                password = @@prompt.ask("Please create a password:", required: true)
+                name = @@prompt.ask("\nWelcome to onboarding. What is your name?\n", required: true)
+                username = @@prompt.ask("\nPlease create a username:\n", required: true)
+                password = @@prompt.ask("\nPlease create a password:\n", required: true)
                 # uses class variabl @@library to input library id and create Student instance
                 @@librarian = Librarian.create(name: name, username: username, password: password, library_id: @@library.id)
-                puts "We hope you have a great first day at the #{@@library.name} library, #{name}!"
+                puts "\nWe hope you have a great first day at the #{@@library.name} library, #{name}!\n"
                 self.librarian_interface
             when "Restart"
                 self.greet_user
@@ -71,37 +71,37 @@ class Cli
 
     def self.student_login
         # Ask if new or returning student
-        new_returning = @@prompt.select("Are you a new or returning student?", ["New", "Returning", "Restart", "Exit"])
+        new_returning = @@prompt.select("\nAre you a new or returning student?\n", ["New", "Returning", "Restart", "Exit"])
             case new_returning
             when "Returning"
                 # Ask for username and make sure it is in system
-                username = @@prompt.ask("Please enter your username:", required: true)
+                username = @@prompt.ask("\nPlease enter your username:\n", required: true)
                 @@student = Student.find_by(username: username)
                 if @@student
                     # Ask for password and make sure it matches username
                     password = @@prompt.mask("Please enter your password:", required: true)
                     if @@student.password == password && @@student.library_id == @@library.id
                         # Go to Student interface
-                        puts "Welcome back to the #{@@library.name} library #{@@student.name}!"
+                        puts "\nWelcome back to the #{@@library.name} library #{@@student.name}!\n"
                         self.student_interface
                     else
                         # Error handling if password doesn't match
-                        puts "Password does not match our records. Please try again."
+                        puts "\nPassword does not match our records. Please try again.\n"
                         self.student_login
                     end
                 else
                     # Error handling if student is not found
-                    puts "Sorry we don't have you in our system. Please register as a new student."
+                    puts "\nSorry we don't have you in our system. Please register as a new student.\n"
                     self.student_login
                 end
             when "New"
                 # input name, username, password
-                name = @@prompt.ask("Lets create an account for you.\n What is your name?", required: true)
-                username = @@prompt.ask("Please create a username:", required: true)
-                password = @@prompt.ask("Please create a password:", required: true)
+                name = @@prompt.ask("\nLets create an account for you.\n What is your name?\n", required: true)
+                username = @@prompt.ask("\nPlease create a username:\n", required: true)
+                password = @@prompt.ask("\nPlease create a password:\n", required: true)
                 # uses class variable @@library to input library id and create Student instance
                 @@student = Student.create(name: name, username: username, password: password, library_id: @@library.id)
-                puts "Thanks for signing up. Enjoy the #{@@library.name} library, #{name}."
+                puts "\nThanks for signing up. Enjoy the #{@@library.name} library, #{name}.\n"
                 self.student_interface
             when "Restart"
                 self.greet_user
@@ -112,23 +112,32 @@ class Cli
 
     def self.librarian_interface
         while true
-            command = @@prompt.select("What would you like to do?", ["See Available Books", "Add Book to Library", "Remove Book from Library", "See Students with Book(s) Checked Out", "Go Home for the Day"])
+            command = @@prompt.select("\nWhat would you like to do?\n", ["See Available Books", "Add Book to Library", "Remove Book from Library", "See Students with Book(s) Checked Out", "Go Home for the Day"])
             case command
             when "See Available Books"
                 # binding.pry
+                puts ''
                 Book.available_books.select {|books| books.library_id == @@library.id}.map {|book| puts book.title}
             when "Add Book to Library"
-                puts "Ok lets add a book to the library! The students will loves this."
+                puts "\nOk lets add a book to the library! The students will loves this.\n"
                 # must be string
-                title = @@prompt.ask("What is the title of this new book?", required: true)
-                author = @@prompt.select("What is the authors name?", Author.all.map{|author| author.name})
-                author_id = Author.find_by(name: author).id
-                genre = @@prompt.select("What is the genre of the book?", Genre.all.map{|genre| genre.genre})
+                title = @@prompt.ask("\nWhat is the title of this new book?\n", required: true)
+                author_prompt = @@prompt.select("\nWhat is the authors name?\n", [*Author.all.map{|author| author.name}, "Not on the List? Add a new author."])
+                if author_prompt == 'Not on the List? Add a new author.' 
+                    author_name = @@prompt.ask("\nEnter a new authors name.\n", required: true)
+                    author = Author.create(name: author_name)
+                else
+                    author = Author.find_by(name: author_prompt)
+                end
+
+                # author_id = Author.find_by(name: author).id
+                genre = @@prompt.select("\nWhat is the genre of the book?\n", Genre.all.map{|genre| genre.genre})
                 genre_id = Genre.find_by(genre: genre).id
+
                 # must be integer
-                pages = @@prompt.ask("How many pages are in this book?", required: true)
-                @@librarian.add_book_to_library(title, author_id, @@library.id, genre_id, pages)
-                puts "Awesome! #{title} has been added to the #{@@library.name} library shelves for the students to enjoy."
+                pages = @@prompt.ask("\nHow many pages are in this book?\n", required: true)
+                @@librarian.add_book_to_library(title, author.id, @@library.id, genre_id, pages)
+                puts "\nAwesome! #{title} has been added to the #{@@library.name} library shelves for the students to enjoy.\n"
             when "Remove Book from Library"
                 puts "Ok, we're sorry you want to remove a book from our shelves."
                 title = @@prompt.select("What is the title of the book you would like to remove?", Book.available_books.map {|book| book.title})
